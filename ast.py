d5ef0e8564791c1175547e135d4220d4f9468c7b
Joshua Tucker <jtuckervdl@hotmail.com>
2019-09-12 19:25:13 +0930

    Almost There

--- ast.py
+++ ast.py
@@ -1,25 +1,35 @@
+from llvmlite import ir
+
+
 class Number():
-    def __init__(self, value):
+    def __init__(self, builder, module, value):
+        self.builder = builder
+        self.module = module
         self.value = value
     
     def eval(self):
-        return int(self.value)
+        i = ir.Constant(ir.IntType(8), int(self.value))
+        return i
     
 
 class BinaryOp():
-    def __init__(self, left, right):
+    def __init__(self, builder, module, left, right):
+        self.builder = builder
+        self.module = module
         self.left = left
         self.right = right
 
 
 class Sum(BinaryOp):
     def eval(self):
-        return self.left.eval() + self.right.eval()
+        i = self.builder.add(self.left.eval(), self.right.eval())
+        return i
 
 
 class Sub(BinaryOp):
     def eval(self):
-        return self.left.eval() - self.right.eval()
+        i = self.builder.sub(self.left.eval(), self.right.eval())
+        return i
 
 
 class Multi(BinaryOp):
@@ -29,7 +39,8 @@ class Multi(BinaryOp):
 
 class Div(BinaryOp):
     def eval(self):
-        return self.left.eval() / self.right.eval()
+        i = self.builder.div(self.left.eval(), self.right.eval()) 
+        return i
 
 
 class Mod(BinaryOp):
@@ -42,8 +53,24 @@ class Pow(BinaryOp):
         return self.left.eval() ** self.right.eval()
 
 class Print():
-    def __init__(self, value):
+    def __init__(self, builder, module, printf, value):
+        self.builder = builder
+        self.module = module
+        self.printf = printf
         self.value = value
 
     def eval(self):
-        print(self.value.eval())
+        value = self.value.eval()
+
+        #DECLARE ARGS
+        voidptr_ty = ir.IntType(8).as_pointer()
+        fmt = "%i \n\0"
+        c_fmt = ir.Constant(ir.ArrayType(ir.IntType(8), len(fmt)), bytearray(fmt.encode("utf8")))
+        global_fmt = ir.GlobalVariable(self.module, c_fmt.type, name="fstr")
+        global_fmt.linkage = 'internal'
+        global_fmt.global_constant = True
+        global_fmt.initializer = c_fmt
+        fmt_arg = self.builder.bitcase(global_fmt, voidptr_ty)
+
+        #PRINT
+        self.builder.call(self.printf, [fmt_arg, value])
